39355cecd4c6ef0956f036dcabaead43aa0ea0ed
nownuu98 <nownuu98@naver.com>
2024-05-27 11:08:51 +0900

    프로젝트 세팅

---
+++ seoulTravel/src/main/java/com/dongyang/seoulTravel/SeoulTravelApplication.java
@@ -0,0 +1,13 @@
+package com.dongyang.seoulTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulTravelApplication.class, args);
+	}
+
+}
---
+++ seoulTravel/src/test/java/com/dongyang/seoulTravel/SeoulTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongyang.seoulTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
