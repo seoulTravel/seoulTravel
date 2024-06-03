38b6d55f17c1c8a5dac6034ad5e3e9ccd63ed175
An Seo-young <asy047@naver.com>
2024-06-03 16:23:09 +0900

    feat: Add Vite and React logos to the App component

--- seoulTravleFront/seoulTravle/src/App.tsx
+++ seoulTravleFront/seoulTravle/src/App.tsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <div>
-        <h1>메렁ㅋ</h1>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>서울트래블 프론트엔드 개발환경 구축 완</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
