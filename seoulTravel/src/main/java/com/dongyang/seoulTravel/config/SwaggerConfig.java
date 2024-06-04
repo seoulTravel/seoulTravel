f061aa5a98a9817878a1aedb263d429985661c72
nownuu98 <nownuu98@naver.com>
2024-06-04 14:19:28 +0900

    Swagger 의존성 추가...

---
+++ seoulTravel/src/main/java/com/dongyang/seoulTravel/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.dongyang.seoulTravel.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SeoulTravel API")
+                        .version("1.0")
+                        .description("서울트래블 API"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-definition")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
