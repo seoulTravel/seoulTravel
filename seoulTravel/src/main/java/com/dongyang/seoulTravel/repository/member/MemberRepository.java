b5a43bdfea2b0817f09b748e3a2194d846b7c6b1
nownuu98 <nownuu98@naver.com>
2024-06-12 12:32:32 +0900

    member 전부 삭제

--- seoulTravel/src/main/java/com/dongyang/seoulTravel/repository/member/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dongyang.seoulTravel.repository.member;
-
-import com.dongyang.seoulTravel.entity.member.Member;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MemberRepository extends CrudRepository<Member, Long> {
-}
-
